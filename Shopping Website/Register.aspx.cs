07acd443614c58b95f04ef91b04341769027d00e
Akshay38744 <85998776+Akshay38744@users.noreply.github.com>
2023-12-13 16:48:07 +0530

    Add files via upload

---
+++ Shopping Website/Register.aspx.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+
+public partial class Default2 : System.Web.UI.Page
+{
+    SqlConnection cn = new SqlConnection(ConfigurationManager.AppSettings["connection"]);
+    SqlCommand cmd;
+    SqlDataAdapter da;
+    DataSet ds;
+    String Today;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        Today = DateTime.Now.ToString();
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+        //try
+        //{
+
+            cn.Open();
+            cmd = new SqlCommand("Proc_register", cn);
+            cmd.CommandType = CommandType.StoredProcedure;
+            cmd.Parameters.Add(new SqlParameter("@mode", SqlDbType.VarChar, 20));
+            cmd.Parameters.Add("@Fname", txtfname.Value);
+            cmd.Parameters.Add("@Lname", txtlname.Value);
+            cmd.Parameters.Add("@Username", Username.Value);
+            cmd.Parameters.Add("@Email", Email.Value);
+            cmd.Parameters.Add("@Add1", Add1.Value);
+            cmd.Parameters.Add("@Add2", Add2.Value);
+            cmd.Parameters.Add("@Mobile", Mobile.Value);
+            cmd.Parameters.Add("@Gender", RadioButtonList1.SelectedValue);
+            cmd.Parameters.Add("@Password", Password.Value);
+            cmd.Parameters.Add("@Confpassword", Conformpwd.Value);
+            cmd.Parameters.Add("@Subdate", Today);
+            cmd.Parameters["@mode"].Value = "Adduser";
+            cmd.ExecuteNonQuery();
+            string msg = "<script>alert('Registered Sucessfully')</script>";
+            Response.Write(msg);
+            cn.Close();
+       // }
+        //catch(Exception ex)
+        //{
+
+        //}
+        //finally
+        //{
+        //    cn.Close();
+        //} 
+    }
+     protected void Button2_Click(object sender, EventArgs e)
+    {
+        txtfname.Value = "";
+        txtlname.Value = "";
+        Username.Value = "";
+        Email.Value = "";
+        Add1.Value = "";
+        Add2.Value ="";
+        Mobile.Value = "";
+        RadioButtonList1.SelectedIndex = 0;
+        Password.Value = "";
+        Conformpwd.Value = "";
+
+    }
+}
+
---
+++ Shopping Website/login.aspx.cs
@@ -0,0 +1,92 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using System.Data.SqlClient;
+using System.Data;
+using System.Configuration;
+using System.Web.Security;
+using System.Text;
+
+public partial class login : System.Web.UI.Page
+{
+    SqlConnection cn = new SqlConnection(ConfigurationManager.AppSettings["connection"]);
+    SqlCommand cmd;
+    SqlCommand com;
+    protected void Page_Load(object sender, EventArgs e)
+    {
+        if (!(Page.IsPostBack))
+        {
+            FillCapctha();
+        }
+
+     
+    }
+    protected void Button1_Click(object sender, EventArgs e)
+    {
+
+        string s = "select * from Users";
+        SqlCommand com = new SqlCommand(s, cn);
+        cn.Open();
+        
+        SqlDataReader dtr;
+        dtr = com.ExecuteReader();
+        while (dtr.Read())
+        {
+            if (dtr[3].ToString().Equals(txtusername.Value) && dtr[9].ToString().Equals(txtpasssword.Value))                
+            {
+                Session["uname"] = txtusername.Value;
+                Session["pwd"] = txtpasssword.Value;
+                Session["Role"] = dtr[12].ToString();
+                FormsAuthentication.RedirectFromLoginPage(txtusername.Value, false);
+                if (dtr[12].ToString() == "Admin")
+                {
+                    Response.Redirect("Admin.aspx");
+                }
+                else
+                {
+                    Response.Redirect("Products.aspx");
+                }
+                Response.Redirect("Products.aspx");
+            }
+            else
+            {
+                lblerror.Text = "Invalid Username and Password";
+            }
+        }
+    }
+
+    public void FillCapctha()
+    {
+        try
+        {
+            Random random = new Random();
+            string combination = "0123456789ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+            StringBuilder captcha = new StringBuilder();
+            for (int i = 0; i < 6; i++)
+            captcha.Append(combination[random.Next(combination.Length)]);
+            Session["captcha"] = captcha.ToString();
+            imgCaptcha.ImageUrl = "Generate-captcha.aspx?" + DateTime.Now.Ticks.ToString();
+        }
+        catch
+        {
+
+
+            throw;
+        }
+    }
+
+    protected void Button2_Click(object sender, EventArgs e)
+    {
+        Response.Write(User.ToString());
+    }
+    protected void Button2_Click1(object sender, EventArgs e)
+    {
+        txtusername.Value = "";
+        txtpasssword.Value = "";
+        lblerror.Text = "";
+       
+    }
+}
